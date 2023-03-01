ad09173a8dcb907ea9093a88b5190d039513ee23
swatiitobuz <swati@itobuz.com>
2023-03-01 18:49:26 +0530

    commited

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 async function weatherReport(){
     const response = await fetch("http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=London&aqi=no");
-    const result = await response.json();
-    console.log(result);
-    const {name} = result;
-    console.log(name);
+    return await response.json();
+    // console.log(result);
+    // const {name} = result;
+    // console.log(name);
 }
-weatherReport();
-
+let result = weatherReport()
+.then((res) => {
+    console.log(res);
+    console.log(res.location);
+    document.getElementById("city").innerHTML = res.location.name;
+    document.getElementById("temparature").innerHTML = res.current.temp_c + "<sup>o<sup>";
+})
