7cf1aa2ff0806f57412d8f98eb68ef0dc7654a86
swatiitobuz <swati@itobuz.com>
2023-03-22 12:15:49 +0530

    22.03.2023

--- js/main.js
+++ js/main.js
@@ -2,89 +2,81 @@ var urlapi = "";
 const temp = document.getElementById("temparature");
 const feelsLike = document.getElementById("feels-like");
 const image = document.getElementById("weather-icon");
-const setcity= document.getElementById("setCity");
+const setcity = document.getElementById("setCity");
 const listItems = document.querySelector("ul");
-async function weatherIcon(){
-    const response = await fetch(urlapi);
+async function weatherIcon() {
+  const response = await fetch(urlapi);
 
-    return await response.json();
+  return await response.json();
 }
 let createIcon = (image) => {
-    let weatherIcon = document.getElementById("weatherIcon");
-    while (weatherIcon.firstChild) {
-      weatherIcon.removeChild(weatherIcon.firstChild);
-    }
-    let elem = document.createElement("img");
-    elem.setAttribute("src", image);
-    elem.setAttribute("alt", "Weather Icon");
-    document.getElementById("weatherIcon").appendChild(elem);
-  };
+  let weatherIcon = document.getElementById("weatherIcon");
+  while (weatherIcon.firstChild) {
+    weatherIcon.removeChild(weatherIcon.firstChild);
+  }
+  let elem = document.createElement("img");
+  elem.setAttribute("src", image);
+  elem.setAttribute("alt", "Weather Icon");
+  document.getElementById("weatherIcon").appendChild(elem);
+};
 let getData = () => {
-    
-    let value = setcity.value;
-    urlapi = "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=" + value + "&aqi=no";
-    console.log(urlapi);
-    let res=weatherIcon()
-    .then((response)=>{
-        console.log(response);
-        document.getElementById("temparature").innerHTML = response.current.temp_f + "\u00B0";
-        document.getElementById("feels-like").innerHTML = "feels  " + response.current.feelslike_f + "\u00B0";
-        console.log(response.current.condition.text);
-        let x = response.current.condition.text;
-        if(x == "Partly cloudy"){
-            createIcon('./images/partly-cloudy.png');
-        }
-        else if(x == "Mist"){
-            createIcon('./images/fog.png');
-        }
-        else if(x == "Stormy"){
-            createIcon('images/stormy.png');
-        }
-        else if(x == "Rainy"){
-            createIcon('./images/clouds.png');
-        }
-        else if(x == "Sprite"){
-            createIcon('./images/sun-clouds.png');
-        }
-        else if(x == "Overcast"){
-            createIcon('./images/sun-clouds-rain.png');
-        }
-        else if(x == 'Sunny'){
-            createIcon('./images/sun.png');
-        }
-        else if(x == "Light rain"){
-            createIcon('./images/rainy.png');
-        }
-        else if(x == "Clear"){
-            createIcon('./images/clear-cloudy.png');
-        }
-        else{
-            createIcon("./images/sun.png")
-        }
-    })
-}
-document.addEventListener('keyup',(eve)=>{
-    if(eve.key==="Enter"){
-        console.log(eve);
-        getData();
+  let value = setcity.value;
+  urlapi =
+    "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=" +
+    value +
+    "&aqi=no";
+  console.log(urlapi);
+  let res = weatherIcon().then((response) => {
+    console.log(response);
+    document.getElementById("temparature").innerHTML =
+      response.current.temp_f + "\u00B0";
+    document.getElementById("feels-like").innerHTML =
+      "feels  " + response.current.feelslike_f + "\u00B0";
+    console.log(response.current.condition.text);
+    let x = response.current.condition.text;
+    if (x == "Partly cloudy") {
+      createIcon("./images/partly-cloudy.png");
+    } else if (x == "Mist") {
+      createIcon("./images/fog.png");
+    } else if (x == "Stormy") {
+      createIcon("images/stormy.png");
+    } else if (x == "Rainy") {
+      createIcon("./images/clouds.png");
+    } else if (x == "Sprite") {
+      createIcon("./images/sun-clouds.png");
+    } else if (x == "Overcast") {
+      createIcon("./images/sun-clouds-rain.png");
+    } else if (x == "Sunny") {
+      createIcon("./images/sun.png");
+    } else if (x == "Light rain") {
+      createIcon("./images/rainy.png");
+    } else if (x == "Clear") {
+      createIcon("./images/clear-cloudy.png");
+    } else {
+      createIcon("./images/sun.png");
     }
-})
-function dropDown(){
-    listItems.classList.toggle("dropdown-toggle");
+  });
+};
+document.addEventListener("keyup", (eve) => {
+  if (eve.key === "Enter") {
+    console.log(eve);
+    getData();
+  }
+});
+function dropDown() {
+  listItems.classList.toggle("dropdown-toggle");
 }
 
 var items = document.querySelectorAll("#list li");
-for(let i = 0;i < items.length;i++){
-    items[i].onclick = function(){
-        document.getElementById("setCity").value = this.innerHTML;
-        getData();
-        dropDown();
-    }
+for (let i = 0; i < items.length; i++) {
+  items[i].onclick = function () {
+    document.getElementById("setCity").value = this.innerHTML;
+    getData();
+    dropDown();
+  };
 }
 window.addEventListener("click", (e) => {
-    if(e.target === setcity ){
-        listItems.classList.remove("dropdown-toggle");
-    }
-    else
-    listItems.classList.add("dropdown-toggle");
-})
+  if (e.target === setcity) {
+    listItems.classList.remove("dropdown-toggle");
+  } else listItems.classList.add("dropdown-toggle");
+});
